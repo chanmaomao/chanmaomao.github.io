93e5bc252588e397cbcbeeff6fdf78e953d61a2b
czz <czz@czzdeMac-mini.local>
2026-05-25 16:28:52 +0800

    Bust QuickLink Pages asset cache

--- assets/channel-DHRY2ew7.js
+++ assets/channel-DHRY2ew7.js
@@ -1,1 +1,1 @@
-import{aS as o,aT as n}from"./index-BwMU5XkA.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
+import{aS as o,aT as n}from"./index-BwMU5XkA-qkfix1.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
