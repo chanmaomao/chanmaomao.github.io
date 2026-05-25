37a4e2b3d984d53c839e4ed0d844d525aaae5935
czz <czz@czzdeMac-mini.local>
2026-05-25 17:14:19 +0800

    Clone Codework API frontend

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# 快连API
+# Codework API（内部站点）
 
-GitHub Pages frontend for 快连API.
+GitHub Pages frontend for Codework API（内部站点）.
 
 Backend API: https://4851ca0ddc43a7a5-111-203-200-194.serveousercontent.com
--- assets/chunk-XZIHB7SX-8DTuk3Y-.js
+++ assets/chunk-XZIHB7SX-8DTuk3Y-.js
@@ -1,1 +1,1 @@
-import{_ as s}from"./index-BwMU5XkA-qkfix1.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};
+import{_ as s}from"./index-BwMU5XkA-cw1.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};
